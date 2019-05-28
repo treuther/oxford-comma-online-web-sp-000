18cbbf2743ca8974106fe743ff02b22d665e8289
Tracy Reuther <tracyreuther@gmail.com>
2019-05-28 00:45:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+if array.length == 1
+  "#{array[0]}"
+elsif array.length == 2
+  "#{array[0]} and #{array.last}"
+else
+  "#{array[0..-2].join(", ")}, and #{array.last}"
+end
 end
